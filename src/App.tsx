d14f042e44027c4292e3e1dc9de0804015a4bb40
Pavel <pavel_chermyanin@mail.ru>
2023-10-08 11:44:03 +0500

    добавили основные переменные(цвета, размеры шрифтов), создали темы, создали контекст(так же хук), реализовали переключение тем и хранение их в localStorage

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
 import { Suspense } from "react";
 import { Link } from "react-router-dom";
 import { Route, Routes } from "react-router-dom";
-import "./index.scss";
+import "./styles/index.scss";
 import { AboutPageAsync } from "./pages/AboutPage/AboutPage.async";
 import { MainPageAsync } from "./pages/MainPage/MainPage.async";
+import { useTheme } from "./theme/useTheme";
 
 export const App = () => {
+  const { theme, toggleTheme } = useTheme();
+
   return (
-    <div className="app">
+    <div className={`app ${theme}`}>
+      <button onClick={toggleTheme}>TOGGLE</button>
       <Link to={`/`}>Главная</Link>
       <Link to={`/about`}>О сайте</Link>
       <Suspense fallback={<div>Loading...</div>}>
