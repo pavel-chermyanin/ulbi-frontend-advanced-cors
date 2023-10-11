1ec6615287c211d7ea1f0d4bef258d144286cf27
Pavel <pavel_chermyanin@mail.ru>
2023-10-11 23:16:38 +0500

    подключили bundle анализатор

--- .eslintrc.js
+++ .eslintrc.js
@@ -34,7 +34,7 @@ module.exports = {
         'import/no-extraneous-dependencies': 'off',
         'no-underscore-dangle': 'off',
         'linebreak-style': 0,
-        'max-len': ['error', { ignoreComments: true }],
+        'max-len': ['error', { ignoreComments: true, code: 100 }],
     },
     globals: {
         __IS_DEV__: true,
